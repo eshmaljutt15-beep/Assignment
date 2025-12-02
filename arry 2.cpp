faa03d4a8fa176fc67e9cbf538e67dbd561e8dd8
Eshmal Jutt <eshmaljutt15@gmail.com>
2025-12-02 16:25:00 +0500

    Add files via upload

---
+++ arry 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+    int a[5];
+    for(int i=0;i<5;i++)
+	 cin >> a[i];
+
+    for(int i=0;i<5;i++)
+        for(int j=i+1;j<5;j++)
+            if(a[i] < a[j])
+                swap(a[i], a[j]);
+
+    for(int i=0;i<5;i++)
+	 cout << a[i] << " ";
+}
+
---
+++ arry 4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+    int a[5];
+    for(int i=0;i<5;i++)
+	 cin >> a[i];
+    for(int i=0;i<5;i++)
+        for(int j=i+1;j<5;j++)
+            if(a[j] < a[i]) swap(a[i], a[j]);
+    for(int i=0;i<5;i++)
+	 cout << a[i] << " ";
+}
+
